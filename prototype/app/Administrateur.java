4056b92e635708d8ae8825ce1304c57e76979000
fallmorningstar <96605068+fallmorningstar@users.noreply.github.com>
2022-06-28 16:32:47 -0400

    Add files via upload

--- prototype/app/Administrateur.java
+++ prototype/app/Administrateur.java
@@ -1,2 +1,50 @@
-public class Administrateur {
+public class Administrateur extends Equipier {
+
+	/**
+	 * 
+	 * @param equipier
+	 * @param role
+	 */
+	public void assignerRole(Equipier equipier, Role role) {
+		// TODO - implement Administrateur.assignerRole
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param equipier
+	 * @param role
+	 */
+	public void modifierRole(Equipier equipier, Role role) {
+		// TODO - implement Administrateur.modifierRole
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param activite
+	 */
+	public void supprimerActivite(Activite activite) {
+		// TODO - implement Administrateur.supprimerActivite
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param compte
+	 */
+	public void suspendreCompte(Compte compte) {
+		// TODO - implement Administrateur.suspendreCompte
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param compte
+	 */
+	public void reactiverCompte(Compte compte) {
+		// TODO - implement Administrateur.reactiverCompte
+		throw new UnsupportedOperationException();
+	}
+
 }
--- prototype/app/Article.java
+++ prototype/app/Article.java
@@ -1,2 +1,33 @@
 public class Article {
+
+	private String lienArticle;
+	private String datePublication;
+	private String lienPublication;
+	private Equipier[] auteurs;
+	private Equipier[] collaborateurs;
+	private String[] motcles;
+	private articleStatus status;
+	private String resume;
+
+	/**
+	 * 
+	 * @param titreOuAuteurOuMotCle
+	 */
+	public void chercherArticle(String titreOuAuteurOuMotCle) {
+		// TODO - implement Article.chercherArticle
+		throw new UnsupportedOperationException();
+	}
+
+	public String getLienArticle() {
+		return this.lienArticle;
+	}
+
+	/**
+	 * 
+	 * @param lienArticle
+	 */
+	public void setLienArticle(String lienArticle) {
+		this.lienArticle = lienArticle;
+	}
+
 }

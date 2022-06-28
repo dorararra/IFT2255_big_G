2014a03e0a53d0b3090018d36c593bec6b1de252
fallmorningstar <96605068+fallmorningstar@users.noreply.github.com>
2022-06-28 16:33:14 -0400

    Add files via upload

--- prototype/app/Compte.java
+++ prototype/app/Compte.java
@@ -1,2 +1,10 @@
 public class Compte {
+
+	private String adresseCourriel;
+	private String motDePasse;
+	private CompteStatus status;
+	private Equipier membre;
+	private Role role;
+	private String numCompte;
+
 }

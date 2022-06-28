ec78fdd9b3100ce71aeaa60ed30393e3e83f6236
fallmorningstar <96605068+fallmorningstar@users.noreply.github.com>
2022-06-28 16:31:54 -0400

    Add files via upload

--- prototype/app/Activite.java
+++ prototype/app/Activite.java
@@ -1,2 +1,49 @@
 public class Activite {
+
+	private String id;
+	private String Titre;
+	private Interet[] interetLie;
+
+	public Activite[] afficherList() {
+		// Show list of activite
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param article
+	 */
+	
+	public Boolean isArticle(String article) {
+			// Determine if this activite is an article
+
+
+
+	/**
+	 * 
+	 * @param outil
+	 */
+	public Boolean isOutil(String outil) {
+		// TODO - implement Activite.isOutil
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param projet
+	 */
+	public Boolean isProjet(String projet) {
+		// TODO - implement Activite.isProjet
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param activite
+	 */
+	public Article afficheFicheComplet(String activite) {
+		// TODO - implement Activite.afficheFicheComplet
+		throw new UnsupportedOperationException();
+	}
+
 }

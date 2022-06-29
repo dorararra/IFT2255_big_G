40b3cd5a0441267e0bc0d66d26bd0bfea18ef13f
UCMP13753 <ucmp13753@gmail.com>
2022-06-28 22:37:02 -0400

    interet

--- prototype/app/Interet.java
+++ prototype/app/Interet.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+
 public class Interet {
 
 	private String id;
 	private String titre;
 	private String description;
+	private ArrayList<Equipier> membreSouscrit;
+	private ArrayList<Activite> activiteSouscrit;
 
 	/**
 	 * 
@@ -10,7 +14,14 @@ public class Interet {
 	 */
 	public Activite[] afficherActiviteAssocieInteret(String interet) {
 		// TODO - implement Interet.afficherActiviteAssocieInteret
+		int num = 0;
+		for(Activite act : this.activiteSouscrit){
+			num ++;
+			System.out.print(num + "." + act.getTitre());
+
+		}
 		throw new UnsupportedOperationException();
+
 	}
 
 	/**
@@ -19,7 +30,36 @@ public class Interet {
 	 */
 	public Equipier[] afficherMembreAssocieInteret(String interet) {
 		// TODO - implement Interet.afficherMembreAssocieInteret
+		int num = 0;
+		for(Equipier mem : this.membreSouscrit){
+			num ++;
+			System.out.print(num + "." + mem.getPrenom() + " " + mem.getNomDeFamille());
+		}
+
 		throw new UnsupportedOperationException();
 	}
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }

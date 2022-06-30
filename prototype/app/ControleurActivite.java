10e4911464a66ede9cfffc1f0bea22ed80a06b9c
qianyunshen <qianyunshen123@163.com>
2022-06-30 17:09:26 -0400

    change controleurActivite

--- prototype/app/ControleurActivite.java
+++ prototype/app/ControleurActivite.java
@@ -1,20 +1,47 @@
-public class ControleurActivite implements Controller {
+import java.io.Serializable;
+import java.util.HashMap;
 
-	private RepertoireActivite repertoire;
+public class ControleurActivite implements Serializable, Controller {
+    private Article article;
+    private Outil outil;
+    private Projet projet;
 
-	public Boolean afficherList() {
-		// TODO - implement ControleurActivite.afficherList
-		throw new UnsupportedOperationException();
+	private RepertoireActivite repertoire = new RepertoireActivite();
+
+    public void ajouteArticle(){
+        repertoire.ajouteArticle(article);
+        System.out.println("Article a déjà ajouté");
+        System.out.println("ID: "+article.getId());
+    }
+    public  void ajouteProjet(){
+        repertoire.ajouteProjet(projet);
+        System.out.println("Projet a déjà ajouté");
+        System.out.println("ID: "+ projet.getId());
+    }
+    public  void ajouteOutil(){
+        repertoire. ajouteOutil(outil);
+        System.out.println("Outil a déjà ajouté ");
+        System.out.println("ID: "+ outil.getId());
+    }
+
+
+	public void afficherListDeArticle() {
+		repertoire.afficherListToutArticle();
 	}
+    public void afficherListToutOutil(){
+        repertoire.afficherListToutOutil();
+    }
+    public void afficherListToutProjet(){
+        repertoire.afficherListToutProjet();
+    }
 
 	public Boolean afficherFicheComplet() {
 		// TODO - implement ControleurActivite.afficherFicheComplet
 		throw new UnsupportedOperationException();
 	}
 
-	public void operation() {
-		// TODO - implement ControleurActivite.operation
-		throw new UnsupportedOperationException();
+	public void chercherArticle(Article article) {
+
 	}
     /**
      *

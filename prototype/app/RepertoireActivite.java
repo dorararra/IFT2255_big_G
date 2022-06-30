8464196d01cb84a66a99d51c93cc0f1c855a2d8f
qianyunshen <qianyunshen123@163.com>
2022-06-30 17:18:13 -0400

    change ControleurActivite et RepertoireActivite
    set uo id

--- prototype/app/RepertoireActivite.java
+++ prototype/app/RepertoireActivite.java
@@ -1,42 +1,79 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class RepertoireActivite {
-    private ArrayList<Activite> activites = new ArrayList<>();
-	private ArrayList<Article> articles = new ArrayList<>();
-	private ArrayList<Outil> outils = new ArrayList<>();
+    private ArrayList<Article> articles = new ArrayList<>();
+    private ArrayList<Outil> outils = new ArrayList<>();
     private ArrayList<Projet> projets = new ArrayList<>();
+    private ArrayList<String> tousLesID = new ArrayList<>();
 
-    public void ajouteActivite(Activite activite){
-        activites.add(activite);
-
+    //chaque ID compose 1 lettre et 6 chiffre
+    private String generate6Chiffre(){
+        Random rand = new Random();
+        String num = "";
+        for(int i = 0;i<6;i++){
+            num += rand.nextInt(10);
+        }
+        return num;
+    }
+    public String generateArticleId() {
+        String idNum = generate6Chiffre();
+        if (!tousLesID.contains(idNum)) {
+            this.tousLesID.add(idNum);
+            String id = 'A' + idNum;
+            return id;
+        } else return generateArticleId();
     }
     public void ajouteArticle(Article article){
+        article.setId(generateArticleId());
         articles.add(article);
     }
+    public  String generateOutilId(){
+        String idNum = generate6Chiffre();
+        if (!tousLesID.contains(idNum)) {
+            this.tousLesID.add(idNum);
+            String id = 'O' + idNum;
+            return id;
+        } else return generateOutilId();
+    }
+
+
     public void ajouteOutil(Outil outil){
+        outil.setId(generateOutilId());
         outils.add(outil);
     }
+
+    public  String generateProjetId(){
+        String idNum = generate6Chiffre();
+        if (!tousLesID.contains(idNum)) {
+            this.tousLesID.add(idNum);
+            String id = 'P' + idNum;
+            return id;
+        } else return generateProjetId();
+    }
     public void ajouteProjet(Projet projet){
+        projet.setId(generateProjetId());
         projets.add(projet);
     }
     public void afficherListToutArticle(){
         int n = 0;
-        for(int i = 0;i<articles.size();i++){
-            System.out.println(n+" . "+articles.get(i));
+        for (Article article : articles) {
+            n++;
+            System.out.println(n + " . " + article);
         }
     }
     public  void afficherListToutOutil(){
         int n = 0;
-        for(int i = 0;i<outils.size();i++){
-            System.out.println(n+ " . "+outils.get(i));
+        for (Outil outil : outils) {
+            n++;
+            System.out.println(n + " . " + outil);
         }
     }
     public void afficherListToutProjet(){
         int n = 0;
-        for(int i = 0; i<projets.size();i++){
-            System.out.println(n+" . "+ projets.get(i));
+        for (Projet projet : projets) {
+            n++;
+            System.out.println(n + " . " + projet);
         }
     }
-
-
 }
